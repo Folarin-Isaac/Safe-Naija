dc2ae4178a2c9bb00dd1b91e3f8a6588ae54b0a1
Folarin36 <ifolarin@vasconsolutions.com>
2021-08-08 00:45:42 +0100

    Implemented the splash screen for the app

--- app/src/main/java/com/example/safenaija/SplashScreen.kt
+++ app/src/main/java/com/example/safenaija/SplashScreen.kt
@@ -1,11 +1,23 @@
 package com.example.safenaija
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
+    //Loading time for the splash screen
+    private val SPLASH_TIMEOUT: Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIMEOUT)
     }
 }
